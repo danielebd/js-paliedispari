c72c1eaaecc3e98ce44972703313327bea9fbbc4
danielebd <danielebalebalestro@gmail.com>
2023-03-01 18:05:08 +0100

    palindroma

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const parola = prompt('inserisci parola');
+    console.log(parola);
+
+function palindromo(text){
+
+    let risultato = false;
+    let dritto = '';
+
+    let i = 0;
+    while(i < text.length){
+        dritto += text[i];
+        i++;
+    }
+    //console.log(dritto);
+
+    let rovescio = '';
+
+    let a = text.length -1;
+    while(a >= 0){
+        rovescio += text[a];
+        //console.log(rovescio);
+        a--;
+    }
+    //console.log(rovescio);
+
+
+    if(dritto === rovescio){
+        risultato = true
+    }
+    return ('la parola è palindroma? ' + risultato)
+}
+alert(palindromo(parola));
+

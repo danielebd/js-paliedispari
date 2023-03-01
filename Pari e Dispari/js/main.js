f0756ae89320a0dc3ef8f74198200cd5f40e4145
danielebd <danielebalebalestro@gmail.com>
2023-03-01 18:40:37 +0100

    scelta pari o dispari

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let inputChoice = '';
+let computerChoise = 'dispari';
+const button = document.getElementById('btn');
+
+button.addEventListener("click", function () {
+
+    //SCELTA PARI O DISPARI
+    inputChoice = document.getElementById('choose').value;
+
+    if (inputChoice === 'dispari') {
+        computerChoise = 'pari'
+    }
+    console.log('utente ' + inputChoice);
+    console.log('computer ' + computerChoise);
+
+    
+})
